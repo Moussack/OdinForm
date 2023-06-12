e7105dd5550ec3e07c08cbbd81a12569193e5fff
moussack <reywahyono@gmail.com>
2023-06-12 12:14:08 +0700

    add password matching

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(`firsts`);
+const password = document.querySelector('#pass');
+const confirmPassword = document.querySelector('#cpass');
+const signUpButton = document.querySelector('.myButton');
+const msg = document.querySelector('#message');
+
+signUpButton.addEventListener('click', (e) => {
+   if (password.value === '' || confirmPassword.value === '') {
+      console.log(`PLEASE INPUT PASS`);
+   } else if (password.value === confirmPassword.value) {
+      console.log(`MATCHED`);
+      msg.textContent = 'MATCHED';
+      msg.style.color = 'blue';
+   } else {
+      msg.textContent = 'DIDNT MATCHED';
+      msg.style.color = 'red';
+      e.preventDefault();
+   }
+});
