7bdff8bb3b43debc0f9d9aec614a1952d58f4c6a
moussack <reywahyono@gmail.com>
2023-06-12 12:34:51 +0700

    add password matching

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ const msg = document.querySelector('#message');
 
 signUpButton.addEventListener('click', (e) => {
    if (password.value === '' || confirmPassword.value === '') {
-      console.log(`PLEASE INPUT PASS`);
+      msg.textContent = 'IS NOT CONFIRMED';
+      msg.style.color = 'gray';
+      e.preventDefault();
+      setTimeout(() => {
+         msg.textContent = '';
+      }, 2500);
    } else if (password.value === confirmPassword.value) {
       console.log(`MATCHED`);
-      msg.textContent = 'MATCHED';
+      msg.textContent = 'IS MATCHED';
       msg.style.color = 'blue';
+      setTimeout(() => {
+         msg.textContent = '';
+      }, 2500);
    } else {
-      msg.textContent = 'DIDNT MATCHED';
+      msg.textContent = 'IS NOT MATCHED';
       msg.style.color = 'red';
       e.preventDefault();
+      setTimeout(() => {
+         msg.textContent = '';
+      }, 2500);
    }
 });
